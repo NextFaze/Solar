7c82052d9ab78a8a4bc8c677e2a0a6535cd64ffd
Dan Silk <dsilk@nextfaze.com>
2016-04-12 18:58:34 +0930

    Updates for Swift 3 - removes var from normalise function

--- Source/Solar.swift
+++ Source/Solar.swift
@@ -27,19 +27,19 @@
 import Foundation
 
 public final class Solar: NSObject {
-    
+
     /// The timezone for the Solar object
     public private(set) var timeZone: NSTimeZone = NSTimeZone.localTimeZone()
-    
+
     /// The latitude that is used for the calculation
     public private(set) var latitude: Double = 0
-    
+
     /// The longitude that is used for the calculation
     public private(set) var longitude: Double = 0
-    
+
     /// The date to generate sunrise / sunset times for
     public private(set) var date: NSDate
-    
+
     public private(set) var sunrise: NSDate? = nil
     public private(set) var sunset: NSDate? = nil
     public private(set) var civilSunrise: NSDate? = nil
@@ -48,49 +48,49 @@ public final class Solar: NSObject {
     public private(set) var nauticalSunset: NSDate? = nil
     public private(set) var astronomicalSunrise: NSDate? = nil
     public private(set) var astronomicalSunset: NSDate? = nil
-    
+
     /// Whether the location specified by the `latitude` and `longitude` is in daytime on `date`
     /// - Complexity: O(1)
     public var isDaytime: Bool {
         let beginningOfDay = sunrise?.timeIntervalSince1970
         let endOfDay = sunset?.timeIntervalSince1970
         let currentTime = NSDate().timeIntervalSince1970
-        
+
         if currentTime >= beginningOfDay && currentTime <= endOfDay {
             return true
         }
         return false
     }
-    
+
     /// Whether the location specified by the `latitude` and `longitude` is in nighttime on `date`
     /// - Complexity: O(1)
     public var isNighttime: Bool {
         return !isDaytime
     }
-    
+
     // MARK: Init
-    
+
     public init?(forDate date: NSDate = NSDate(), withTimeZone timeZone: NSTimeZone = NSTimeZone.localTimeZone(), latitude: Double, longitude: Double) {
         self.date = date
         self.timeZone = timeZone
         self.latitude = latitude
         self.longitude = longitude
         super.init()
-        
+
         guard latitude >= -90.0 && latitude <= 90.0 else {
             return nil
         }
-        
+
         guard longitude >= -180.0 && longitude <= 180.0 else {
             return nil
         }
-        
+
         // Fill this Solar object with relevant data
         calculate()
     }
-    
+
     // MARK: - Public functions
-    
+
     /// Sets all of the Solar object's sunrise / sunset variables, if possible.
     /// - Note: Can return `nil` objects if sunrise / sunset does not occur on that day.
     public func calculate() {
@@ -103,14 +103,14 @@ public final class Solar: NSObject {
         astronomicalSunrise = calculate(.Sunrise, forDate: date, andZenith: .Astronimical)
         astronomicalSunset = calculate(.Sunset, forDate: date, andZenith: .Astronimical)
     }
-    
+
     // MARK: - Private functions
-    
+
     private enum SunriseSunset {
         case Sunrise
         case Sunset
     }
-    
+
     /// Used for generating several of the possible sunrise / sunset times
     private enum Zenith: Double {
         case Official = 90.83
@@ -118,107 +118,110 @@ public final class Solar: NSObject {
         case Nautical = 102
         case Astronimical = 108
     }
-    
+
     private func calculate(sunriseSunset: SunriseSunset, forDate date: NSDate, andZenith zenith: Zenith) -> NSDate? {
         // Get the day of the year
         let calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
         calendar.timeZone = timeZone
         let day = Double(calendar.ordinalityOfUnit(.Day, inUnit: .Year, forDate: date))
-        
+
         // Convert longitude to hour value and calculate an approx. time
         let lngHour = longitude / 15
-        
+
         let hourTime: Double = sunriseSunset == .Sunrise ? 6 : 18
         let t = day + ((hourTime - lngHour) / 24)
-        
+
         // Calculate the suns mean anomaly
         let M = (0.9856 * t) - 3.289
-        
+
         // Calculate the sun's true longitude
         var L = M + (1.916 * sin(M.degreesToRadians)) + (0.020 * sin(2 * M.degreesToRadians)) + 282.634
-        
+
         // Normalise L into [0, 360] range
         L = normalise(L, withMaximum: 360)
-        
+
         // Calculate the Sun's right ascension
         var RA = atan(0.91764 * tan(L.degreesToRadians)).radiansToDegrees
-        
+
         // Normalise RA into [0, 360] range
         RA = normalise(RA, withMaximum: 360)
-        
+
         // Right ascension value needs to be in the same quadrant as L...
         let Lquadrant = floor(L / 90) * 90
         let RAquadrant = floor(RA / 90) * 90
         RA = RA + (Lquadrant - RAquadrant)
-        
+
         // Convert RA into hours
         RA = RA / 15
-        
+
         // Calculate Sun's declination
         let sinDec = 0.39782 * sin(L.degreesToRadians)
         let cosDec = cos(asin(sinDec))
-        
+
         // Calculate the Sun's local hour angle
         let cosH = (cos(Zenith.Official.rawValue.degreesToRadians) - (sinDec * sin(latitude.degreesToRadians))) / (cosDec * cos(latitude.degreesToRadians))
-        
+
         // No sunrise
         guard cosH < 1 else {
             return nil
         }
-        
+
         // No sunset
         guard cosH > -1 else {
             return nil
         }
-        
+
         // Finish calculating H and convert into hours
         let tempH = sunriseSunset == .Sunrise ? 360 - acos(cosH).radiansToDegrees : acos(cosH).radiansToDegrees
         let H = tempH / 15.0
-        
+
         // Calculate local mean time of rising
         let T = H + RA - (0.06571 * t) - 6.622
-        
+
         // Adjust time back to UTC
         var UT = T - lngHour
-        
+
         // Normalise UT into [0, 24] range
         UT = normalise(UT, withMaximum: 24)
-        
+
         // Convert UT value to local time zone of lat/long provided
         let localT = UT + (Double(timeZone.secondsFromGMT) / 3600.0)
-        
+
         let hour = floor(localT)
         let minute = floor((localT - hour) * 60.0)
         let second = (((localT - hour) * 60) - minute) * 60.0
-        
+
         let components = calendar.components([.Day, .Month, .Year], fromDate: date)
         components.hour = Int(hour)
         components.minute = Int(minute)
         components.second = Int(second)
-        
+
         var date = calendar.dateFromComponents(components)
-        
+
         // Add a day to account for potential timezone issues
         if sunriseSunset == .Sunset && date?.timeIntervalSince1970 < sunrise?.timeIntervalSince1970 {
             date = date?.dateByAddingTimeInterval(60 * 60 * 24)
         }
-        
+
         return date
     }
-    
+
     /// Normalises a value between 0 and `maximum`, by adding or subtracting `maximum`
-    private func normalise(var value: Double, withMaximum maximum: Double) -> Double {
+    private func normalise(value: Double, withMaximum maximum: Double) -> Double {
+
+        var normalisedValue = value
+
         if value < 0 {
-            value += maximum
+            normalisedValue += maximum
         }
-        
+
         if value > maximum {
-            value -= maximum
+            normalisedValue -= maximum
         }
-        
-        return value
+
+        return normalisedValue
     }
-    
+
 }
 
 // MARK: - Helper extensions
@@ -227,7 +230,7 @@ private extension Double {
     var degreesToRadians: Double {
         return Double(self) * (M_PI / 180.0)
     }
-    
+
     var radiansToDegrees: Double {
         return (Double(self) * 180.0) / M_PI
     }
